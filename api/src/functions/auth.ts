6ca1a899e594ce1b7273bd1807c81786b00c8776
Fred Zhao <frdzy@users.noreply.github.com>
2022-12-21 16:41:09 -0600

    fix webAuthn

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -190,11 +190,11 @@ export const handler = async (
       domain:
         process.env.NODE_ENV === 'development'
           ? 'localhost'
-          : 'lets-go-play-tennis-api.onrender.com',
+          : 'lets-go-play-tennis-web.onrender.com',
       origin:
         process.env.NODE_ENV === 'development'
           ? 'http://localhost:8910'
-          : 'https://lets-go-play-tennis-api.onrender.com',
+          : 'https://lets-go-play-tennis-web.onrender.com',
       type: 'platform',
       timeout: 60000,
       credentialFields: {
