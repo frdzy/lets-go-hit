2cad4f63892ddecd43e29bb5739bac4692b7e96b
Fred Zhao <frdzy@users.noreply.github.com>
2022-12-23 22:37:13 -0600

    add an attempt to auto-detect render.com config issue

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,28 @@
+import { useAuth } from '@redwoodjs/auth';
 import { MetaTags } from '@redwoodjs/web';
+import { toast, Toaster } from '@redwoodjs/web/toast';
+import { useEffect } from 'react';
 import PlayersCell from 'src/components/PlayersCell';
 
 const HomePage = () => {
+  const auth = useAuth();
+
+  // Notifier for render.com issue to poke the rewrite rules
+  // https://community.redwoodjs.com/t/seeing-authorization-header-is-not-valid-after-deploying-to-render-com/2808
+  useEffect(() => {
+    (async () => {
+      const token = await auth.getToken();
+      if (token.includes('<')) {
+        toast.error('App error');
+      }
+    })();
+  }, [auth]);
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       <h2 className="rw-heading rw-heading-primary my-2">Home</h2>
+      <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
       <PlayersCell />
     </>
   );
