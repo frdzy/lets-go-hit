4f6ac42ba6eb24944fb92a0474d1e79c6175cdb0
Fred Zhao <frdzy@users.noreply.github.com>
2023-01-02 18:20:46 -0800

    remove auto-detection logic, since getToken() is always returning null

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -5,19 +5,6 @@ import { useEffect } from 'react';
 import PlayersCell from 'src/components/PlayersCell';
 
 const HomePage = () => {
-  const auth = useAuth();
-
-  // Notifier for render.com issue to poke the rewrite rules
-  // https://community.redwoodjs.com/t/seeing-authorization-header-is-not-valid-after-deploying-to-render-com/2808
-  useEffect(() => {
-    (async () => {
-      const token = await auth.getToken();
-      if (token.includes('<')) {
-        toast.error('App error');
-      }
-    })();
-  }, [auth]);
-
   return (
     <>
       <MetaTags title="Home" description="Home page" />
