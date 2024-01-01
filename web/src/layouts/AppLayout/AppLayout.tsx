ebac59562d4a89865f1f380d23dcaac37f25c98c
Fred Zhao <frdzy@users.noreply.github.com>
2024-01-01 14:39:19 -0600

    improve chrome rendering on mobile

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -26,27 +26,28 @@ const AppLayout = ({ children }: AppLayoutProps) => {
     },
   ];
 
+  const loginStyle = 'mx-2 my-auto max-w-xs rounded-xl bg-slate-50 p-2';
   return (
     <>
       <header className="sm:flex">
-        <h1 className="font-xl my-auto px-4 pt-2 text-xl">
-          Let's Go Play Tennis
-        </h1>
+        <h1 className="font-xl my-auto px-2 pt-2 text-xl">Let's Go Play 🎾</h1>
         <div className="grow" />
-        <div className="mx-2 my-auto max-w-xs rounded-xl bg-slate-50 p-2">
-          {isAuthenticated && currentUser ? (
-            <>
-              <span>Logged in as {currentUser.email}</span>{' '}
-              <button type="button" onClick={logOut}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <NavLink activeClassName="activeLink" to={routes.login()}>
-              Login
-            </NavLink>
-          )}
-        </div>
+        {isAuthenticated && currentUser ? (
+          <>
+            <span className="p-2">{currentUser.email}</span>
+            <button className={loginStyle} type="button" onClick={logOut}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <NavLink
+            className={loginStyle}
+            activeClassName="activeLink"
+            to={routes.login()}
+          >
+            Login
+          </NavLink>
+        )}
       </header>
       <nav className="mx-3 my-2 flex sm:justify-center">
         {navItems.map((o) => (
